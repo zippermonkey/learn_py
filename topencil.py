d27a44f24d187f1a9fc798ae538e0458612740e8
zippermonkey <1960727314@qq.com>
2020-03-29 16:08:22 +0800

    转手绘

---
+++ topencil.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 19:43:11 2020
+
+@author: zippermonkey
+"""
+
+
+print("hello world")
+
+# from list or tuple
+
+import numpy as np
+from PIL import Image
+
+depth = 10
+
+a = np.array(Image.open("165.png").convert("L")).astype('float')
+
+grad = np.gradient(a)
+grad_x, grad_y = grad
+grad_x = grad_x*depth/100
+grad_y = grad_y*depth/100
+A = np.sqrt(grad_x**2+grad_y**2+1.)
+uni_x = grad_x/A
+uni_y = grad_y/A
+uni_z = 1./A
+
+vec_el = np.pi/2.2
+vec_az = np.pi/4.
+dx = np.cos(vec_el)*np.cos(vec_az)
+dy = np.cos(vec_el)*np.sin(vec_az)
+dz = np.sin(vec_el)
+
+b = 255*(dx*uni_x+dy+uni_y+dz*uni_z)
+b = b.clip(0,255)
+
+im = Image.fromarray(b.astype('uint8'))
+im.save('165hd.jpg')
